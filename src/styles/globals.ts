3cdd1853803bc55839fdce0b1708d64af62c379f
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2023-04-10 20:03:22 -0300

    Login e menu

---
+++ src/styles/globals.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+	* {
+		box-sizing: border-box;
+		padding: 0;
+		margin: 0;
+	}
+
+	html,
+	body {
+		max-width: 100vw;
+		overflow-x: hidden;
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+`;
