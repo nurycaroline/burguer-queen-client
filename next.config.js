595a781e8e5337f811356619979eecd3a8bb61d6
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2023-04-24 16:09:55 -0300

    forceSwcTransforms

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true
-  }
+  },
+  experimental: {
+    forceSwcTransforms: true,
+  },
 }
 
 module.exports = nextConfig
